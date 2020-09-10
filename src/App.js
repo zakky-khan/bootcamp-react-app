ea18b9ecd0425b655ed6326bd7f42ce8c7bd3515
zakky-khan <zakky-khan@users.github.com>
2020-09-09 21:32:15 -0400

    <dinner/>
    creating tag <dinnner/>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dinner from './dinner.js' ;
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
            <code>hello  world from zakky</code> z.
+           <Dinner />
         </p>
         <a
           className="App-link"
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Dinner />
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Dinner(){
+    return(
+        <div>
+            <h1>today we are serving chicken baryani</h1>
+            <h1>today we are serving chicken karahi</h1>
+            
+        </div>
+    )
+}
+
+export default Dinner ;
